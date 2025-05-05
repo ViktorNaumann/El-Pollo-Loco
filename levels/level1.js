a7cd370cd103d05059385da2ef2fe43b6a65cb78
Viktor Naumann <Viktor_Naumann@hotmail.de>
2025-05-05 13:20:28 +0200

    added more clouds and speed fixed

--- levels/level1.js
+++ levels/level1.js
@@ -1,6 +1,22 @@
 const level1 = new Level(
   [new Chicken(), new Chicken(), new Chicken(), new Endboss()],
-  [new Cloud()],
+  [
+    new Cloud(Math.random() * 3595),
+    new Cloud(Math.random() * 3595),
+    new Cloud(Math.random() * 3595),
+    new Cloud(Math.random() * 3595),
+    new Cloud(Math.random() * 3595),
+    new Cloud(Math.random() * 3595),
+    new Cloud(Math.random() * 3595),
+    new Cloud(Math.random() * 3595),
+    new Cloud(Math.random() * 3595),
+    new Cloud(Math.random() * 3595),
+    new Cloud(Math.random() * 3595),
+    new Cloud(Math.random() * 3595),
+    new Cloud(Math.random() * 3595),
+    new Cloud(Math.random() * 3595),
+    new Cloud(Math.random() * 3595)
+  ],
   [
     new BackgroundObject("img/5_background/layers/air.png", -719),
     new BackgroundObject("img/5_background/layers/3_third_layer/2.png", -719),
--- models/cloud.class.js
+++ models/cloud.class.js
@@ -3,14 +3,31 @@ class Cloud extends MovableObject {
   width = 450;
   height = 250;
 
-  constructor() {
-    super().loadImage("img/5_background/layers/4_clouds/1.png");
-    this.x = -200 + Math.random() * 500;
+  constructor(x) {
+    const images = [
+      "img/5_background/layers/4_clouds/1.png",
+      "img/5_background/layers/4_clouds/2.png"
+    ];
+    super();
+    const selectedImage = images[Math.floor(Math.random() * images.length)];
+    this.loadImage(selectedImage);
+  
+    this.x = x;
+    this.y = 20;
+    this.width = 450;
+    this.height = 250;
+    this.speed = 0.05 + Math.random() * 0.25; // ergibt 0.05–0.3
     this.animate();
   }
+  
 
   animate() {
-    this.moveLeft();
+    const move = () => {
+      this.moveLeft();
+      requestAnimationFrame(move);
+    };
+    move();
   }
+  
   
 }
