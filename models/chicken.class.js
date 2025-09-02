d1510e6eadf246d66f5f824a6fd325ff4ec119d9
Viktor Naumann <Viktor_Naumann@hotmail.de>
2025-09-02 14:44:59 +0200

    refactor Chicken class constructor and movement logic for improved structure and readability

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -34,9 +34,27 @@ class Chicken extends MovableObject {
    */
   constructor(isBig = false, x = 200 + Math.random() * 3300) {
     super();
+    this.initializePosition(x);
+    this.initializeMovementProperties();
+    this.setupChickenType(isBig);
+    this.loadImages(this.IMAGES_DEAD);
+    this.animate();
+  }
+
+  /**
+   * Initializes chicken position and speed
+   * @param {number} x - Initial x position
+   */
+  initializePosition(x) {
     this.x = x;
     this.speed = 0.15 + Math.random() * 0.25;
     this.otherDirection = Math.random() < 0.5;
+  }
+
+  /**
+   * Initializes movement behavior properties
+   */
+  initializeMovementProperties() {
     this.changeDirectionTime = Date.now();
     this.minDirectionTime = 3000 + Math.random() * 2000;
     this.maxDirectionTime = 5000 + Math.random() * 3000;
@@ -44,48 +62,92 @@ class Chicken extends MovableObject {
     this.pauseDuration = 1000 + Math.random() * 2000;
     this.isPaused = false;
     this.lastPauseTime = 0;
+  }
 
+  /**
+   * Sets up chicken size and appearance
+   * @param {boolean} isBig - Whether this is a big chicken
+   */
+  setupChickenType(isBig) {
     if (isBig) {
-      this.height = 80;
-      this.width = 80;
-      this.y = 390 - (this.height - 40);
-      this.loadImage(this.IMAGES_BIG[0]);
-      this.loadImages(this.IMAGES_BIG);
-      this.animationSet = this.IMAGES_BIG;
+      this.setupBigChicken();
     } else {
-      this.height = 40;
-      this.width = 40;
-      this.y = 390;
-      this.loadImage(this.IMAGES_SMALL[0]);
-      this.loadImages(this.IMAGES_SMALL);
-      this.animationSet = this.IMAGES_SMALL;
+      this.setupSmallChicken();
     }
+  }
 
-    this.loadImages(this.IMAGES_DEAD);
-    this.animate();
+  /**
+   * Configures big chicken properties
+   */
+  setupBigChicken() {
+    this.height = 80;
+    this.width = 80;
+    this.y = 390 - (this.height - 40);
+    this.loadImage(this.IMAGES_BIG[0]);
+    this.loadImages(this.IMAGES_BIG);
+    this.animationSet = this.IMAGES_BIG;
+  }
+
+  /**
+   * Configures small chicken properties
+   */
+  setupSmallChicken() {
+    this.height = 40;
+    this.width = 40;
+    this.y = 390;
+    this.loadImage(this.IMAGES_SMALL[0]);
+    this.loadImages(this.IMAGES_SMALL);
+    this.animationSet = this.IMAGES_SMALL;
   }
 
   /**
    * Sets up animation and movement intervals
    */
   animate() {
-    this.moveInterval = setInterval(() => {
-      if (this.otherDirection) {
-        this.moveRight();
-      } else {
-        this.moveLeft();
-      }
-      let currentTime = Date.now();
-      if (currentTime - this.changeDirectionTime > this.getRandomDirectionTime()) {
-        this.otherDirection = Math.random() < 0.6 ? !this.otherDirection : this.otherDirection;
-        this.changeDirectionTime = currentTime;
-        this.speed = 0.15 + Math.random() * 0.25;
-      }
-    }, 1000 / 60);
+    this.startMovementInterval();
+    this.startAnimationInterval();
+  }
 
+  /**
+   * Starts the movement interval for chicken
+   */
+  startMovementInterval() {
+    this.moveInterval = setInterval(() => {
+      this.handleMovement();
+      this.checkDirectionChange();
+    }, 1000 / 60);
+  }
+
+  /**
+   * Starts the animation interval for chicken
+   */
+  startAnimationInterval() {
     this.animationInterval = setInterval(() => {
       this.playAnimation(this.animationSet);
     }, 150);
+  }
+
+  /**
+   * Handles chicken movement based on direction
+   */
+  handleMovement() {
+    if (this.otherDirection) {
+      this.moveRight();
+    } else {
+      this.moveLeft();
+    }
+  }
+
+  /**
+   * Checks and handles direction changes
+   */
+  checkDirectionChange() {
+    let currentTime = Date.now();
+    if (currentTime - this.changeDirectionTime > this.getRandomDirectionTime()) {
+      this.otherDirection = Math.random() < 0.6 ? !this.otherDirection : this.otherDirection;
+      this.changeDirectionTime = currentTime;
+      this.speed = 0.15 + Math.random() * 0.25;
+    }
   }
 
   /**
@@ -125,7 +187,6 @@ class Chicken extends MovableObject {
     clearInterval(this.moveInterval);
     clearInterval(this.animationInterval);
     this.speed = 0;
-
     if (this.height > 40) {
       this.loadImage(this.IMAGES_BIG_DEAD[0]);
     } else {
@@ -135,4 +196,3 @@ class Chicken extends MovableObject {
     this.isDead = true;
   }
 }
-
